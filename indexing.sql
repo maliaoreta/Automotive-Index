6e9cd2c30c4c3419b9e2558329c2da4d0d1ea8e5
maliaoreta <maliaoreta@gmail.com>
2016-04-16 19:57:58 -1000

    timing select statements 3

--- indexing.sql
+++ indexing.sql
@@ -5,3 +5,7 @@
 
 -- SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
 -- runtime w/o index 34.817
+
+-- SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+-- runtime w/o index 31.489 ms
+
