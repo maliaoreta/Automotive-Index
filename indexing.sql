c9795724feecf7e2f8b92e962f2a3730fec546e1
maliaoreta <maliaoreta@gmail.com>
2016-04-16 20:02:48 -1000

    indexing 2

--- indexing.sql
+++ indexing.sql
@@ -9,6 +9,7 @@
 
 -- SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
 -- runtime w/o index 34.817
+-- runtime w/index 2.838
 
 -- SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
 -- runtime w/o index 31.489 ms
