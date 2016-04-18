fb6f1cd17e3635eaa272fef6eab6bfad5daabe36
maliaoreta <maliaoreta@gmail.com>
2016-04-18 09:12:35 -1000

    runs first statement with index in script file"

--- indexing.sql
+++ indexing.sql
@@ -6,6 +6,7 @@
 -- SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
 -- runtime w/o index 34.526 ms
 -- runtime w/index 1.648 ms
+-- runtime w/index in script 6.458 ms
 
 -- SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
 -- runtime w/o index 34.817
